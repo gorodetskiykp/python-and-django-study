8e96143b65150b2027f8951ebe3d2ab651ae6edf
GorodetskiyKP <gorodetskiykp@dmn.transneft.ru>
2017-10-03 00:56:35 +1000

    Поправил задание 2.4 Нетологии по замечаниям

--- netology.ru_PY8/hw_2.4/find_procedure.py
+++ netology.ru_PY8/hw_2.4/find_procedure.py
@@ -1,40 +1,51 @@
 import os
+import re
 
 migrations = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
+abs_path = os.path.join(current_dir,  migrations)
 
-def files_reader(abs_path):
-    # Принимает абсолютный путь до директории
-    # Возвращает список файлов в директории
+def files_reader():
+    """Возвращает список sql-файлов в директории."""
     all_files = os.listdir(abs_path)
-    return [file_name for file_name in all_files if '.sql' in file_name]
+    return [file_name for file_name in all_files if file_name.endswith('.sql')]
 
-def find_word(file_name, word, abs_path):
-    # Принимает имя файла и искомое слово
-    # Можно передать абсолютный путь до файла
-    # По умолсанию ищет в текущей директории
-    # Возвращает True, если слово найдено в файле
+def find_word(file_name, word, case_insensitive=True):
+    """Ищет слово в файле.
+    Возвращает True, если слово найдено.
+    Аргументы:
+    file_name - файл, в котором нужно искать
+    word - слово, которое нужно искать
+    case_insensitive - регистронезависимый поиск (по умолчанию True)
+    """
     with open(os.path.join(abs_path, file_name), 'r', encoding='cyrillic') as parsed_file:
-        return word in parsed_file.read()
+        text = parsed_file.read()
+        text = re.sub(r'\W', '', text)
+        if case_insensitive:
+            return word.lower() in text.lower()
+        else:
+            return word in text
 
-def find_files_in_list(files_list, word, abs_path=''):
-    # Получает список файлов
-    # Возвращает список файлов, в которых найдено слово
+def find_files_in_list(files_list, word):
+    """Фильтрует список файлов по искомому слову.
+    Возвращает отфильтрованный список файлов.
+    Аргументы:
+    file_list - список имен файлов
+    word - слово, по которому идет фильтация списка
+    """
     new_files_list = []
     for file_name in files_list:
-        if find_word(file_name, word, abs_path):
+        if find_word(file_name, word):
             new_files_list.append(file_name)
     return new_files_list
 
 if __name__ == '__main__':
-    abs_path = os.path.join(current_dir, migrations)
-    files = files_reader(abs_path)
+    files = files_reader()
     while True:
         word = input('Введите строку: ')
-        files = find_files_in_list(files, word, abs_path)
+        files = find_files_in_list(files, word)
         print('\n'.join(files))
         result_len = len(files)
         print('Всего: {}'.format(result_len))
         if result_len == 0:
             break
-
